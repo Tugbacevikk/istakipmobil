cbaadbbd1abeff25530fdf2c9ad157eba7e7845d
Tugbacevikk <tcevik2824@gmail.com>
2026-08-20 14:11:59 +0300

    fix: resolve ApiClient import error in AlarmListScreen

--- lib/screens/alarms/alarm_list_screen.dart
+++ lib/screens/alarms/alarm_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../core/constants/app_colors.dart';
+import '../../core/network/api_client.dart';
 import '../../models/alarm.dart';
 import '../../providers/app_provider.dart';
 
